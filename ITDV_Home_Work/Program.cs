659a74000395b5d48d9368f1718e5ac44d6703ae
VladBordiug <vladislav.ukraine@gmail.com>
2021-08-27 14:14:47 -0400

    Add project files.

---
+++ ITDV_Home_Work/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITDV_Home_Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
